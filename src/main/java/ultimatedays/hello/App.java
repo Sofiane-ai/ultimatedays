7d27ce45271c91554da909f3fd4112de79b09797
Sofiane-ai <fadel.soufiane@yahoo.fr>
2020-08-11 10:59:17 +0200

    Evolution de l'interface utilisateur par ajout d'un message plus performant

--- src/main/java/ultimatedays/hello/App.java
+++ src/main/java/ultimatedays/hello/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! Avec ma modification !" );
+        System.out.println( getValue() );
+        
+    }
+    public static String getValue() {
+    	return "Coucou ";
     }
 }
